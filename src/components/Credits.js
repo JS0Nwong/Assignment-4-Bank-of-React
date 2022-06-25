88a73e44defc74c5dd4278be534089a84eb63dad
Jason <Jason.Wong47@myhunter.cuny.edu>
2022-06-25 19:32:23 -0400

    implementing credit and debits functions

---
+++ src/components/Credits.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import AccountBalance from './AccountBalance';
+import { Link } from 'react-router-dom';
+
+
+class Credits extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Credits</h1>
+            </div>
+        );
+    }
+}
+
+export default Credits;
+    
